c355641acd879027ac21af8ebfff66f61008e7dd
YUAN Yu <yuan@archos.com>
2014-03-19 17:29:48 +0100

    [implement] custom application class

---
+++ src/com/yuanyu/placardsmap/CustomApplication.java
@@ -0,0 +1,69 @@
+package com.yuanyu.placardsmap;
+
+import com.baidu.mapapi.BMapManager;
+import com.baidu.mapapi.MKGeneralListener;
+import com.baidu.mapapi.map.MKEvent;
+
+import android.app.Application;
+import android.content.Context;
+import android.widget.Toast;
+
+public class CustomApplication extends Application {
+
+	private static CustomApplication mInstance = null;
+	public boolean m_bKeyRight = true;
+	BMapManager mBMapManager = null;
+
+	public static final String strKey = "D388cdb60225303eb120ef0fe5e78a17";
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		mInstance = this;
+		initEngineManager(this);
+	}
+
+	public void initEngineManager(Context context) {
+		if (mBMapManager == null) {
+			mBMapManager = new BMapManager(context);
+		}
+
+		if (!mBMapManager.init(strKey, new MyGeneralListener())) {
+			Toast.makeText(CustomApplication.getInstance().getApplicationContext(), 
+					"BMapManager  初始化错误!", Toast.LENGTH_LONG).show();
+		}
+	}
+
+	public static CustomApplication getInstance() {
+		return mInstance;
+	}
+
+	// 常用事件监听，用来处理通常的网络错误，授权验证错误等
+	static class MyGeneralListener implements MKGeneralListener {
+
+		@Override
+		public void onGetNetworkState(int iError) {
+			if (iError == MKEvent.ERROR_NETWORK_CONNECT) {
+				Toast.makeText(CustomApplication.getInstance().getApplicationContext(), "您的网络出错啦！",
+						Toast.LENGTH_LONG).show();
+			}
+			else if (iError == MKEvent.ERROR_NETWORK_DATA) {
+				Toast.makeText(CustomApplication.getInstance().getApplicationContext(), "输入正确的检索条件！",
+						Toast.LENGTH_LONG).show();
+			}
+		}
+
+		@Override
+		public void onGetPermissionState(int iError) {
+			//非零值表示key验证未通过
+			if (iError != 0) {
+				//授权Key错误：
+				CustomApplication.getInstance().m_bKeyRight = false;
+			}
+			else{
+				CustomApplication.getInstance().m_bKeyRight = true;
+			}
+		}
+	}
+
+}
